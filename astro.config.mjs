381a9d51b7ba5a1e1d2f4f5c5f9eb5e9f9210256
Gorcea Radu Andrei <80151170+lamoo7@users.noreply.github.com>
2025-09-19 20:45:18 +0300

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
+import react from '@astrojs/react';
+
+export default defineConfig({
+  integrations: [clerk(), react() ],
+  adapter: node({ mode: "standalone" }),
+  output: "server",
+});
